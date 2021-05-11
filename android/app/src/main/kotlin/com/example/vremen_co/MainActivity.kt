4b372840d6e66e7ec16d90a07defd519cf9d4015
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2021-05-11 22:53:31 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/vremen_co/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vremen_co
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen/home_screen_image/home_screen_image.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Widget homeScreenImage() {
+  // Just render an image across the entire width of the screen (from left to right
+  return Image.asset('woman_wearing_headphones_using_laptop.jpeg');
+}
