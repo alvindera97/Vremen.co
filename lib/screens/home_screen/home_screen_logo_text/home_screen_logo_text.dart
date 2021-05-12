dfb5f459e3d5e5fc55300e9fe1edcde16a4216fb
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2021-05-12 07:12:40 +0100

    <Feature> (home, home_screen): Add Secondary Text [under logo] At Home Screen.

--- lib/screens/home_screen/home_screen_logo_text/home_screen_logo_text.dart
+++ lib/screens/home_screen/home_screen_logo_text/home_screen_logo_text.dart
@@ -5,11 +5,26 @@ Widget homeScreenLogoText() {
   * This widget returns the logo text at the home page of the app
   * */
   return Container(
-    padding: EdgeInsets.fromLTRB(0, 39, 0, 0),
-    child: Text(
-      "Vremen.co",
-      style: TextStyle(
-          fontSize: 35, fontWeight: FontWeight.w500, color: Colors.white),
-    ),
-  );
+      padding: EdgeInsets.fromLTRB(0, 120, 0, 0),
+      child: Column(
+        children: [
+          Text(
+            "Vremen.co",
+            style: TextStyle(
+                fontSize: 35, fontWeight: FontWeight.w500, color: Colors.white),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Text(
+              "Almost fun* time-tracking",
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 2.0,
+                  fontFamily: "Open Sans"),
+            ),
+          )
+        ],
+      ));
 }
